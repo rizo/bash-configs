c92d99f78ff6b111079d280c37b0ed8c5bf46e4e
Rizo Isrof <rizo@odis.io>
2013-09-09 02:00:12 +0100

    Nicer (un)installer.

--- install.sh
+++ install.sh
@@ -1,13 +1,40 @@
 
-export MARKPATH=$HOME/.marks
+info () {
+	printf "\e[34m##\e[0m $1\n"
+}
 
+error () {
+	printf "  \e[31m*\e[0m $1\n"
+}
+
+die () {
+	printf "\e[91m✗\e[0m\n\n"
+	exit -1
+}
+
+status () {
+	printf "  \e[33m-\e[0m $1 "	
+}
+
+ok () {
+	printf "\e[92m✓\033[0m\n"
+}
+
+
+info "Bash Dotfiles"
+
+status "Creating marks derictory..."
+export MARKPATH=$HOME/.marks
 if ! [ -d $MARKPATH ]
 then
 	mkdir $MARKPATH
 fi
+ok
 
-ln -s `pwd`/bash_profile $HOME/.bash_profile
-ln -s `pwd`/bashrc $HOME/.bashrc
-ln -s `pwd`/marksrc $HOME/.marksrc
-ln -s `pwd`/aliases $HOME/.aliases
+for dotfile in bash_profile bashrc marksrc aliases; do
+	status "Linking .$dotfile..."
+	ln -s "`pwd`/bash/$dotfile" "$HOME/.$dotfile" 2> /dev/null || die
+	ok
+done
 
+printf "\n"
--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,41 @@
-export MARKPATH=$HOME/.marks
 
+info () {
+	printf "\e[34m##\e[0m $1\n"
+}
+
+error () {
+	printf "  \e[31m*\e[0m $1\n"
+}
+
+die () {
+	printf "\e[91m✗\e[0m\n\n"
+	exit -1
+}
+
+status () {
+	printf "  \e[33m-\e[0m $1 "	
+}
+
+ok () {
+	printf "\e[92m✓\033[0m\n"
+}
+
+
+info "Bash Dotfiles"
+
+status "Removing marks derictory..."
+MARKPATH=$HOME/.marks
 if [ -d $MARKPATH ]
 then
-	rm -r $MARKPATH
+	rm -rf $MARKPATH
 fi
+ok
 
-unlink $HOME/.bash_profile 2> /dev/null
-unlink $HOME/.bashrc 2> /dev/null
-unlink $HOME/.marksrc 2> /dev/null
-unlink $HOME/.aliases 2> /dev/null
+for dotfile in bash_profile bashrc marksrc aliases; do
+	status "Unlinking .$dotfile..."
+	unlink "$HOME/.$dotfile" 2> /dev/null
+	ok
+done
+
+printf "\n"
 
